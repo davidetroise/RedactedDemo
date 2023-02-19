0b59d49ef1e41a8df917a0a6a38b7c3759bbfd8b
Davide Troise <davide@troise.net>
2023-02-19 17:13:10 +0100

    Initial Commit

---
+++ RedactedDemo/RedactedDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  RedactedDemoApp.swift
+//  RedactedDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct RedactedDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
